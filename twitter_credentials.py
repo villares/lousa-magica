4ec444911ece7495d2ac01bd66424d06a80660b8
Alexandre B A Villares <abav@lugaralgum.com>
2017-08-02 18:14:38 -0300

    Create twitter_credentials.py

---
+++ twitter_credentials.py
@@ -0,0 +1,9 @@
+"""
+Replace those constants with your credentials
+But don't publish them on GitHub!
+"""
+
+OACK = "XXX"  # OAuthConsumerKey
+OACS = "YYY"  # OAuthConsumerSecret
+OAAT = "ZZZ"  # OAuthAccessToken
+OATS = "WWW"  # OAuthAccessTokenSecret
