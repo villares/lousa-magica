8f1359b9d4ff70c0a0138e2b93267ebba8162119
Alexandre B A Villares <abav@lugaralgum.com>
2017-09-07 06:48:57 -0300

    Delete twitter_credentials.py

--- twitter_credentials.py
+++
@@ -1,9 +0,0 @@
-"""
-Replace those constants with your credentials
-But don't publish them on GitHub!
-"""
-
-OACK = "XXX"  # OAuthConsumerKey
-OACS = "YYY"  # OAuthConsumerSecret
-OAAT = "ZZZ"  # OAuthAccessToken
-OATS = "WWW"  # OAuthAccessTokenSecret
