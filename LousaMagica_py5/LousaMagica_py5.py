97377b088d8bc7252f54640aa9058cfaa441c39f
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-08-03 18:44:12 -0300

    moving PyFirmata stuff to the get_arduino() helper

--- LousaMagica_py5/LousaMagica_py5.py
+++ LousaMagica_py5/LousaMagica_py5.py
@@ -5,8 +5,6 @@ desenho com potenciômetros
 http://abav.lugaralgum.com/lousa-magica
 """
 
-import pyfirmata
-
 def setup():
     global arduino
     size(700, 700)   # ajuste aqui o tamanho da área de desenho!
@@ -14,17 +12,7 @@ def setup():
     frame_rate(30)
     no_stroke()
     background(0)
-    arduino = pyfirmata.Arduino('/dev/ttyUSB0')
-    pyfirmata.util.Iterator(arduino).start()
-    for i in range(6):
-        arduino.analog[i].enable_reporting()
-    arduino.analog_read = (lambda p: arduino.analog[p].read() * 1024
-                           if arduino.analog[p].read() is not None
-                           else 0)
-    digital_pins = {13: arduino.get_pin('d:13:i')}
-    for pin in digital_pins.values():
-        pin.enable_reporting()
-    arduino.digital_read = lambda p: digital_pins[p].read()    
+    arduino = get_arduino(0)  # Mude a porta aqui se necessário!   
 
 def draw():
     pot_1 = arduino.analog_read(1)  # pino A1 (Analógico 1)
@@ -43,4 +31,48 @@ def draw():
     # Note modo HSB no setup! (Matiz, Saturação, Brilho, Alfa)
     fill(F % 256, sat, 255, opa)  
     ellipse(x, y, tam, tam)
-    #println(pot_1, pot_2, pot_3, pot_4, tilt)
+
+def get_arduino(port=None):
+    """
+    This is a PyFirmata 'helper' that tries to connect to
+    an Arduino compatible board.
+    
+    If port is None it tries to connect to the last port
+    listed by pyserial's serial.tools.list_ports.comports().
+    You can provide a string with the port name or an integer
+    index to the port (as listed by pyserial and printed in
+    the console if no port is provided)
+    
+    If successful it returns a pyfirmata Arduino object, but
+    before that it starts a pyfirmata.util.Iterator, and adds
+    to it analog_read() and digital_read() functions that mimic
+    Processing's Firmata library interface.
+    """
+    from pyfirmata import Arduino, util
+    from serial.tools import list_ports
+    ports = [comport.device for comport in list_ports.comports()]
+    if not ports:
+        raise Exception('No board/Arduino port found')
+    elif port is None:
+        print('\n'.join(f'{i}: {p}' for i, p in enumerate(ports)))
+        port = len(ports) - 1
+    if isinstance(port, str):
+        print(f'Trying to connect to port: {port}')
+        arduino = Arduino(port)
+    else:
+        print(f'Trying to connect to port {port}: {ports[port]}')
+        arduino = Arduino(ports[port])
+    util.Iterator(arduino).start()
+    for a in range(6):  # A0 A1 A2 A3 A4 A5
+        arduino.analog[a].enable_reporting()
+    arduino.analog_read = (lambda a: arduino.analog[a].read() * 1024
+                           if arduino.analog[a].read() is not None
+                           else 0)
+    digital_pin_dict = {d: arduino.get_pin(f'd:{d}:i')
+                        for d in range(2, 14)}
+    for d in digital_pin_dict.keys():
+        digital_pin_dict[d].enable_reporting()
+    arduino.digital_read = (lambda d: digital_pin_dict[d].read()
+                            if digital_pin_dict[d].read() is not None
+                            else False)
+    return arduino
