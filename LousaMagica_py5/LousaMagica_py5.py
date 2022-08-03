5a339c9503d093a43e1352fa457fb30754041370
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2022-08-03 15:57:11 -0300

    back with digital read code

--- LousaMagica_py5/LousaMagica_py5.py
+++ LousaMagica_py5/LousaMagica_py5.py
@@ -9,29 +9,30 @@ import pyfirmata
 
 def setup():
     global arduino
-    size(700, 700)  # ajuste aqui o tamanho da área de desenho!
+    size(700, 700)   # ajuste aqui o tamanho da área de desenho!
     color_mode(HSB)  # para usar HSB em vez de RGB!
     frame_rate(30)
     no_stroke()
     background(0)
     arduino = pyfirmata.Arduino('/dev/ttyUSB0')
     pyfirmata.util.Iterator(arduino).start()
-    for i in range(1, 5):
+    for i in range(6):
         arduino.analog[i].enable_reporting()
     arduino.analog_read = (lambda p: arduino.analog[p].read() * 1024
                            if arduino.analog[p].read() is not None
                            else 0)
-    #digital_pins = {13: arduino.get_pin('d:13:i')}
-    #arduino.digital_read = lambda p: digital_pins[p].read()    
+    digital_pins = {13: arduino.get_pin('d:13:i')}
+    for pin in digital_pins.values():
+        pin.enable_reporting()
+    arduino.digital_read = lambda p: digital_pins[p].read()    
 
 def draw():
     pot_1 = arduino.analog_read(1)  # pino A1 (Analógico 1)
     pot_2 = arduino.analog_read(2)
     pot_3 = arduino.analog_read(3)
     pot_4 = arduino.analog_read(4)
-    # tilt = arduino.digital_read(13)  # pino 13 (Digital 13)
-    #if tilt or is_key_pressed:
-    if is_key_pressed:
+    tilt = arduino.digital_read(13) # pino 13 (Digital 13)
+    if tilt or is_key_pressed:
         background(0)  # limpa o canvas com preto
     x = remap(pot_1, 0, 1023, 0, width)
     y = remap(pot_4, 0, 1023, 0, height)
