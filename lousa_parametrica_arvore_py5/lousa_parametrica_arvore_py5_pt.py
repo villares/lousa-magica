6a67860d93776d5dfb9a6f35cab972c0c12a18ae
Alexandre B A Villares <3694604+villares@users.noreply.github.com>
2023-06-11 00:53:37 -0300

    Versão da árvore com py5 e código em um arquivo só.

--- lousa_parametrica_arvore_py5/lousa_parametrica_arvore_py5_pt.py
+++ lousa_parametrica_arvore_py5/lousa_parametrica_arvore_py5_pt.py
@@ -1,14 +1,14 @@
 """
-Alexandre B A Villares
-árvore recursiva - Circuito SESC de arte 2018 - v2022_08_06
-http://abav.lugaralgum.com/lousa-magica
+Alexandre B A Villares - http://abav.lugaralgum.com/lousa-magica
+Árvore Recursiva - Circuito SESC de arte 2018
+v2023_06_10 - Versão com py5 e código em um arquivo só.
 
 Para executar é necessário:
-py5 (Usando Thonny IDE com o thonny-py5mode ative o *imported mode*)
-PyFirmata 
+No computador, bibliotecas py5 e PyFirmata, Thonny IDE
+com o thonny-py5mode ativando o *imported mode*.
+No Arduino, ligado em uma porta USB, o código FirmataAllImputs carregado
+e conexão das portas analógias A1, A2, A3 e A4 a 4 potenciômetros.
 """
-
-from inputs import get_arduino
 
 def setup():
     global arduino
@@ -16,9 +16,8 @@ def setup():
     color_mode(HSB)
     arduino = get_arduino()
     if arduino is None:
-        print('Could not connect to an Arduino compatible board')
+        print('Não consegui conectar ao Arduino.')
         exit_sketch()
-    # Or try port name like get_arduino('COM3') or '/dev/ttyUSB0' 
  
 def draw():
     global a, b, c, d
@@ -61,4 +60,81 @@ def galho(gen, theta, galho_size):
 
 def key_pressed():
     if key == 'p':
-        save_frame("lousa-02-####.png")
+        save_frame("imagem####.png")
+
+def get_arduino(port=None):
+    """Função ajudante para conectar em um Arduino usando PyFirmata."""   
+    from pyfirmata import Arduino, util
+    from serial.tools import list_ports
+    
+    comports = [comport.device for comport in list_ports.comports()]
+    if not comports:
+        print('Nenhoma porta de conexão encontrada.')
+        return None
+    elif isinstance(port, str) and port not in comports:
+        print(f'Port "{port}" não encontrada.')
+        return None
+    elif isinstance(port, int):
+        if port >= len(comports):
+            print(f'Port [{port}] não encontrada.')
+            return None
+        else:
+            port = comports[port]
+    elif len(comports) == 1:
+        port = comports[0]
+    elif port is None:
+        port = option_pane(
+            'Em qual entrada está o seu Arduino?',
+            'Selecione a porta USB:',
+            comports,
+            -1)  # index for default option
+        if port is None:
+            print('Nenhuma porta selecionada.')
+            return None
+    try:
+        print(f'Conectando na porta {port}...')
+        arduino = Arduino(port)
+        util.Iterator(arduino).start()
+    except Exception as e:
+        print(repr(e))
+        return None
+    # Prepare analog_read() for A0 A1 A2 A3 A4 A5
+    for a in range(6):  
+        arduino.analog[a].enable_reporting()
+    arduino.analog_read = (lambda a: round(arduino.analog[a].read() * 1023)
+                           if arduino.analog[a].read() is not None
+                           else 0)
+    # Prepare digital_read() for D2 to D13
+    digital_pin_dict = {d: arduino.get_pin(f'd:{d}:i')
+                        for d in range(2, 14)}
+    for d in digital_pin_dict.keys():
+        digital_pin_dict[d].enable_reporting()
+    arduino.digital_read = (lambda d: digital_pin_dict[d].read()
+                            if digital_pin_dict[d].read() is not None
+                            else False)
+    return arduino
+
+
+def option_pane(title, message, options, default=None, index_only=False):
+    """Java swing JOptionPane input dialog with drop down options."""
+    from javax.swing import JOptionPane
+    
+    if default is None:
+        default = options[0]
+    elif index_only:
+        default = options[default]
+
+    selection = JOptionPane.showInputDialog(
+        None,     # frame
+        message,
+        title,
+        JOptionPane.INFORMATION_MESSAGE,
+        None,     # for Java null
+        options,
+        default)  # must be in options, otherwise first is shown
+    if selection:
+        if index_only:
+            return options.index(selection)
+        else:
+            # Trouble: selection can be java.lang.String
+            return str(selection) if selection else None
